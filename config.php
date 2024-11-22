30e9fbe9caa1896ca7ed579f2756687c70029150
maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>
2024-11-22 07:47:27 +0000

    Add files via upload
    Signed-off-by: maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
-// Define server details needed to connect to the database
-$servername = 'localhost'; // The server where the database is hosted
-$username = 'root';  //    // The username to access the database
-$password = '';      //pass      
-$dbname = 'user_database'; 
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = 'sustainify';
 
-// Attempt to connect to the database using the provided details
-$conn = mysqli_connect($servername, $username, $password, $dbname) 
-    or die('Unable to connect'); // If the connection fails, display an error message and stop execution
-
-// Check if the connection has an error and handle it
-if ($conn->connect_error) {
-    die('Connection failed'); // If there’s an error, stop execution and show 'Connection failed'
-} else {
-    // Connection was successful, so no further action is needed';
+function connect_db(){
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+    // Checking connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    return $conn;
 }
 ?>
