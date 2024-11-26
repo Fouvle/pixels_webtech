2761d66a1d447b9d31e46eafcdf50b9714ba19c8
maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>
2024-11-26 12:10:46 +0000

    Update and rename Login.html to Login.php
    Signed-off-by: maame-dankwaa <144915734+maame-dankwaa@users.noreply.github.com>

--- view/Login.php
+++ view/Login.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="Login.css">
+    <link rel="stylesheet" href="../assets/login.css">
 </head>
 <body>
     <div class="login-container">
         <form action="login.php" method="POST" class="login-form">
-            <label for="email"><b>Email: </b></label><br>
-            <input type="text" id="email" name="email" placeholder="enter email"><br>
+            <label for="email"><b>Email:</b></label><br>
+            <input type="text" id="email" name="email" placeholder="Enter Email"><br>
+            
             <label for="pwd"><b>Password:</b></label><br>
             <input type="password" id="pwd" name="password" placeholder="Password"><br><br>
+            
             <input type="checkbox" id="remember" name="remember" value="Remember me">
             <label for="remember"><b>Remember me</b></label><br><br>
+            
             <input type="submit" value="Login"><br><br>
-            <p>Don't have an account? <a href="signup.html">Sign Up</a></p>
+            <p>Don't have an account? <a href="register.php">Sign Up</a></p>
         </form>
     </div>
     <script src="login.js"></script>
